f066b0cba1a2f7a2ab3db52420c50f2d4502d5c3
DSUN <dsun@hanmail.net>
2022-09-01 10:59:21 +0900

    초기 커밋.

---
+++ src/AppsettingsJsonFactoryConfig.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MetaFrm.Config
+{
+    /// <summary>
+    /// FactoryConfig를 appsettings.json으로 관리하는 클래스 입니다.
+    /// </summary>
+    public class AppsettingsJsonFactoryConfig : IFactoryConfig
+    {
+        readonly IConfigurationRoot configuration;
+
+        /// <summary>
+        /// AppsettingsJsonFactoryConfig 인스턴스를 생성합니다.
+        /// </summary>
+        public AppsettingsJsonFactoryConfig()
+        {
+            IConfigurationBuilder builder1 = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            this.configuration = builder1.Build();
+        }
+
+        string IFactoryConfig.GetAttribute(ICore core, string attributeName)
+        {
+            return (this as IFactoryConfig).GetAttribute($"{core.GetType().FullName}", attributeName);
+        }
+
+        List<string> IFactoryConfig.GetAttribute(ICore core, List<string> listAttributeName)
+        {
+            List<string> vs = new();
+
+            foreach (var attribute in listAttributeName)
+                vs.Add((this as IFactoryConfig).GetAttribute($"{core.GetType().FullName}", attribute));
+
+            return vs;
+        }
+
+        string IFactoryConfig.GetAttribute(string namespaceName, string attributeName)
+        {
+            //await Task.Run(() => { });
+            return GetValue($"{namespaceName}.{attributeName}") ?? "";
+        }
+
+        string IFactoryConfig.GetPath(string namespaceName)
+        {
+            return GetValue($"{namespaceName}.Path") ?? "";
+        }
+
+        private string? GetValue(string attributeName)
+        {
+            IConfigurationSection? configurationSection;
+            string[] vs;
+
+            configurationSection = null;
+            vs = attributeName.Split('.');
+
+            foreach (string v in vs)
+                if (configurationSection == null)
+                    configurationSection = configuration.GetSection(v);
+                else
+                    configurationSection = configurationSection.GetSection(v);
+
+            if (configurationSection == null)
+                return null;
+            else
+                return configurationSection.Value;
+        }
+    }
+}
